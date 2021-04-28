c30f187d06956ffb192cce58600c283b71dd90d6
seraphlive <12388970+seraphlive@users.noreply.github.com>
2021-04-28 16:07:09 +0800

    Daiy Apr 28 (Add TODO)

---
+++ cpp/dynamic-programming/medium/63unique_paths_ii.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=63 lang=cpp
+ *
+ * [63] Unique Paths II
+ *
+ * https://leetcode.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (35.44%)
+ * Likes:    2668
+ * Dislikes: 293
+ * Total Accepted:    366.3K
+ * Total Submissions: 1M
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * Now consider if some obstacles are added to the grids. How many unique paths
+ * would there be?
+ * 
+ * An obstacle and space is marked as 1 and 0 respectively in the grid.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: 2
+ * Explanation: There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: obstacleGrid = [[0,1],[0,0]]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] is 0 or 1.
+ * 
+ * 
+ */
+// TODO: Unique path series
+// @lc code=start
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+    {
+        
+    }
+};
+// @lc code=end
+
