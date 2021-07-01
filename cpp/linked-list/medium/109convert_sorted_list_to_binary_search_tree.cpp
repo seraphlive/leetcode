d9283237d4e9499cde9354e8b3d845e2185c46fb
seraphlive <12388970+seraphlive@users.noreply.github.com>
2021-07-01 23:12:41 +0800

    Tree-BST 6~7

---
+++ cpp/linked-list/medium/109convert_sorted_list_to_binary_search_tree.cpp
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode id=109 lang=cpp
+ *
+ * [109] Convert Sorted List to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (52.54%)
+ * Likes:    3402
+ * Dislikes: 102
+ * Total Accepted:    317.5K
+ * Total Submissions: 604.3K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given the head of a singly linked list where elements are sorted in
+ * ascending order, convert it to a height balanced BST.
+ *
+ * For this problem, a height-balanced binary tree is defined as a binary tree
+ * in which the depth of the two subtrees of every node never differ by more
+ * than 1.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: One possible answer is [0,-3,9,-10,null,5], which represents
+ * the shown height balanced BST.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [0]
+ * Output: [0]
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: head = [1,3]
+ * Output: [3,1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in head is in the range [0, 2 * 10^4].
+ * -10^5 <= Node.val <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  TreeNode* sortedListToBST(ListNode* head) {
+    if (!head) return nullptr;
+    if (!head->next) return new TreeNode(head->val);
+    auto mid = head;
+    auto pre = head;
+    auto fast = head;
+
+    while (fast && fast->next) {
+      pre = mid;
+      fast = fast->next->next;
+      mid = mid->next;
+    }
+
+    TreeNode* root = new TreeNode(mid->val);
+    auto ne = mid->next;
+    pre->next = nullptr;
+    mid->next = nullptr;
+    root->left = sortedListToBST(head);
+    root->right = sortedListToBST(ne);
+    return root;
+  }
+};
+// @lc code=end
---
+++ cpp/tree/easy/108convert_sorted_array_to_binary_search_tree.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=108 lang=cpp
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (62.00%)
+ * Likes:    4210
+ * Dislikes: 290
+ * Total Accepted:    555.8K
+ * Total Submissions: 896.5K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given an integer array nums where the elements are sorted in ascending
+ * order, convert it to a height-balanced binary search tree.
+ *
+ * A height-balanced binary tree is a binary tree in which the depth of the two
+ * subtrees of every node never differs by more than one.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,3]
+ * Output: [3,1]
+ * Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums is sorted in a strictly increasing order.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return dfs(nums, 0, nums.size() - 1);
+  }
+
+ private:
+  TreeNode* dfs(vector<int>& nums, int l, int r) {
+    if (l > r) return nullptr;
+    int mid = l + (r - l) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = dfs(nums, l, mid - 1);
+    root->right = dfs(nums, mid + 1, r);
+    return root;
+  }
+};
+// @lc code=end
